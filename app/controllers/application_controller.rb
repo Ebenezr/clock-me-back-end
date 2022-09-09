f0ade084a6e5e4cb65d23e76ed88db7e8d96c895
Ebenezr <ebenezarbukosia@gmail.com>
2022-09-09 16:36:57 +0300

    add code documentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  @user
+  @users
   # Add your routes here
+
+
+  # user login authentification
   post '/login/user' do 
    
     user = Employee.find_by(username: params[:username], password: params[:password]) || Admin.find_by(username: params[:username], password: params[:password])
@@ -60,9 +63,7 @@ class ApplicationController < Sinatra::Base
     end
 
   #change user password
-  get '/employee/updatepassword/:email' do
- 
-
+  patch '/employee/updatepassword/:email' do
     user = Employee.find_by(params[:email])
     if user.nil?
       responce ={
@@ -70,16 +71,16 @@ class ApplicationController < Sinatra::Base
       }
       responce.to_json
     else 
-      user.new(password: params[:password])
-    #  user.to_json
-    
+      user.update(password: params[:password])
+     user.to_json
     end  
   
 end
 
   #fetch all employees
   get '/employees/fetch' do
-    Employee.all.to_json
+    @users= Employee.all
+    @users.to_json
   end
 
     #fetch all departments
@@ -101,6 +102,7 @@ end
   end
 
 
+  #fetch employee timestapms
   get '/employee/timestamps/:id' do
     employee = Employee.find(params[:id])
     if !employee.nil? 
@@ -113,6 +115,7 @@ end
     end  
   end
 
+  #update employee's timestamps
   put '/employee/timestamps/update/:id' do
     employee = Employee.find(params[:id])
     if !employee.nil? 
@@ -127,6 +130,7 @@ end
   end
 
 
+  #fetch employee's timerecords
   get '/employee/timerecord/:id' do
     employee = Employee.find(params[:id])
     if !employee.nil? 
@@ -139,6 +143,7 @@ end
     end  
   end
 
+  #write and updates employyes records
   put '/employee/timerecord/update/:id' do
     employee = Employee.find(params[:id])
     if !employee.nil? 
@@ -152,6 +157,7 @@ end
     end  
   end
 
+  #fetch and computes employees total weeky time
   get '/employee/fetch/totalhours/:id' do
     employee = Employee.find(params[:id]);
     hours = employee.timestamp.monday + employee.timestamp.tuesday + employee.timestamp.wednesday + employee.timestamp.thursday + employee.timestamp.friday
@@ -159,12 +165,14 @@ end
   
   end
    
+  #computes and fetch employees avarage hourly time
   get '/employee/fetch/avaragetime/:id' do
     employee = Employee.find(params[:id]);
     hours = employee.timestamp.monday + employee.timestamp.tuesday + employee.timestamp.wednesday + employee.timestamp.thursday + employee.timestamp.friday
     (hours/5).to_json
   end
 
+  #fetch and compute employee salaray based on houry rate
   get '/employee/fetch/salary/:id' do
     rate=850
     tax=85
@@ -174,8 +182,4 @@ end
     (hours*855).to_json
   
   end
-
-
-
-
 end
